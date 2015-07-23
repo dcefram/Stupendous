469623cac3beab7e5895b51bd39b24656e50621d
dcefram <me@danielcefram.com>
2015-07-23 15:05:14 +0800

    style: convert 4 spaces to 2 spaces for both stylus and js files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,39 +1,39 @@
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+  "use strict";
 
-    var $siteHead = "";
-    var $postHead = "";
+  var $siteHead = "";
+  var $postHead = "";
 
-    var resizeHead = function() {
-    	if ($siteHead.length > 0 ||
-            $postHead.length > 0) {
-    		// Elementary grade coding skills LOL
-            if ($siteHead.hasClass("tag"))
-            {
-                $siteHead.css({
-                    "padding" : (window.innerWidth * 0.15625 / 2) + "px 0px " +
-                                (window.innerWidth * 0.15625 / 20) + "px"
-                });
-            }
-            else
-            {
-                $siteHead.css({
-                    "padding" : (window.innerWidth * 0.15625 / 4) + "px 0px"
-                });
-            }
-    		$postHead.css({
-                "padding" : (window.innerWidth * 0.078125 / 2) + "px 0px"
-            });
-    	}
-    };
+  var resizeHead = function() {
+  	if ($siteHead.length > 0 ||
+        $postHead.length > 0) {
+  		// Elementary grade coding skills LOL
+      if ($siteHead.hasClass("tag"))
+      {
+        $siteHead.css({
+          "padding" : (window.innerWidth * 0.15625 / 2) + "px 0px " +
+                      (window.innerWidth * 0.15625 / 20) + "px"
+        });
+      }
+      else
+      {
+        $siteHead.css({
+          "padding" : (window.innerWidth * 0.15625 / 4) + "px 0px"
+        });
+      }
+  		$postHead.css({
+        "padding" : (window.innerWidth * 0.078125 / 2) + "px 0px"
+      });
+  	}
+  };
 
-    $(document).ready(function() {
-        $siteHead = $("#site-head");
-    	$postHead = $(".post-header");
+  $(document).ready(function() {
+    $siteHead = $("#site-head");
+  	$postHead = $(".post-header");
 
-    	resizeHead();
-    });
+  	resizeHead();
+  });
 
-    $(window).resize(resizeHead);
+  $(window).resize(resizeHead);
 }(jQuery));
