d96dba1bbff0786ec0d1757f4e9fba9c6254e83d
dcefram <me@danielcefram.com>
2016-02-25 23:49:51 +0800

    chore: update jeet dependency

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@ let gulp   = require('gulp');
 let stylus = require('gulp-stylus');
 let watch  = require('gulp-watch');
 let batch  = require('gulp-batch');
+let jeet   = require('jeet');
 
 gulp.task('stylus', () => {
   return gulp.src('./assets/stylus/**/*.styl')
-    .pipe(stylus())
+    .pipe(stylus({ use: jeet() }))
     .pipe(gulp.dest('./assets/css'));
 });
 
