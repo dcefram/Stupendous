8c5844f1ebbeb69c695eb006277f0019917aaa7d
Daniel Cefram Ramirez <me@danielcefram.com>
2016-03-01 13:09:18 +0800

    feat: initial restructuring of css and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,18 @@
+'use strict';
+
 let gulp   = require('gulp');
 let stylus = require('gulp-stylus');
 let watch  = require('gulp-watch');
 let batch  = require('gulp-batch');
 let jeet   = require('jeet');
+let nib    = require('nib');
 
 gulp.task('stylus', () => {
-  return gulp.src('./assets/stylus/**/*.styl')
-    .pipe(stylus({ use: jeet() }))
+  return gulp.src('./src/stylus/**/*.styl')
+    .pipe(stylus({
+      use: [jeet(), nib()],
+      import: ['jeet', 'nib']
+    }))
     .pipe(gulp.dest('./assets/css'));
 });
 
